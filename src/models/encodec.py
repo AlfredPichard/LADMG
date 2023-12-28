bc9f3f072c6ea7cbcc7eab480b6bb5ec3b105ff4
Paul Triana <paul.triana@telecom-paris.fr>
2023-12-28 21:34:07 +0100

    encodec wrapper

--- src/models/encodec.py
+++ src/models/encodec.py
@@ -1,46 +1,55 @@
-from datasets import load_dataset, Audio
-from transformers import AutoProcessor, EncodecModel
-import torch
+import encodec
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
-class Encodec(nn.Module):
+class WrappedEncodec(nn.Module):
 
-    def __init__(self, model_id = "facebook/encodec_24khz"):
-        super(Encodec, self).__init__()
-        self.model_id = model_id
-        self.model = EncodecModel.from_pretrained(self.model_id)
-        self.processor = AutoProcessor.from_pretrained(self.model_id)
+    def __init__(self,params_file=None) -> None:
+        super().__init__()
+        self.model = encodec.EncodecModel.encodec_model_24khz()
+        self.sr = 24000
+        self.param = False
+        if params_file is not None:
+            self.param = True
+            pca_params = torch.load(params_file)
+            self.latent_mean = pca_params["mean"]
+            self.components = pca_params["components"]
+            self.n_latents = 64
         
-    def test(self):
-        print('test')
-    
-    def process(self, audio):
-        return self.processor(raw_audio=audio, sampling_rate=self.processor.sampling_rate, return_tensors='pt')
-    
-    def encode(self, inputs):
-        return self.model.encode(inputs["input_values"], inputs["padding_mask"])
-    
-    def decode(self, codes, inputs):
-        return self.model.decode(codes.audio_codes, codes.audio_scales, inputs["padding_mask"])[0]
-    
-    def get_codes(self, inputs):
-        encoded_frames = self.encode(inputs).audio_codes
-        return torch.cat([encoded[0] for encoded in encoded_frames], dim=-1)
+    def truncate(self, z: torch.Tensor):
+        z = z-self.latent_mean.unsqueeze(-1)
+        z = F.conv1d(z,self.components.unsqueeze(-1).to(z))
+        z = z[:,:self.n_latents]
+        return z
 
-    def get_codes_audio(self, audio):
-        inputs = self.process(audio)
-        encoded_frames = self.encode(inputs).audio_codes
-        return torch.cat([encoded[0] for encoded in encoded_frames], dim=-1)
+    def encode(self, x: torch.Tensor, trunc = True) -> torch.Tensor:
+        z,scales = self.model._encode_frame(x)
+        z = z.transpose(0, 1)  # (n_quant, batch, time)
+        z = self.model.quantizer.decode(z)
+        if trunc == True and self.param:
+            z = self.truncate(z)
+        
+        return z
+               
+    def decode(self, z: torch.Tensor,trunc=True) -> torch.Tensor: 
+        if trunc==True and self.param:
+            noise = torch.zeros(z.shape[0], 128-self.n_latents, z.shape[-1]).to(z)
+            z = torch.cat((z, noise),axis=1)
+            z = F.conv1d(z,self.components.T.unsqueeze(-1).to(z)) + self.latent_mean.unsqueeze(-1).to(z)
+            
+        return self.model.decoder(z)
     
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.decode(self.encode(x))
+    
+
 if __name__ == "__main__":
-    model = Encodec()
-    # dummy dataset, however you can swap this with an dataset on the 🤗 hub or bring your own
-    librispeech_dummy = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+    encodec = WrappedEncodec()
 
-    # cast the audio data to the correct sampling rate for the model
-    librispeech_dummy = librispeech_dummy.cast_column("audio", Audio(sampling_rate=model.processor.sampling_rate))
-    audio_sample = librispeech_dummy[10]["audio"]["array"]
+    x = torch.randn(1,1,24000)
 
-    codes = model.get_codes_audio(audio_sample)
-    print(codes.shape)
+    z = encodec.encode(x)[0][0]
+    x_rec = encodec.decode(z) #x_rec = encodec(x)
 
+    print(z.shape,x_rec.shape)
