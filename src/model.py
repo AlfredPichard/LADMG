2686c0c03538bd7abc192975cc2964d0b61b20f3
Paul Triana <paul.triana@telecom-paris.fr>
2023-12-29 21:42:51 +0100

    encodec integré au model pour l'inference

--- src/model.py
+++ src/model.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
 from models.unet import UNet
+from models.encodec_model import WrappedEncodec
 
 class UNetDiffusion(nn.Module):
 
-    def __init__(self, n_channels = 128, alpha_steps = 1000):
+    def __init__(self, n_channels = 128, alpha_steps = 100):
         super(UNetDiffusion, self).__init__()
         self.n_channels = n_channels
         self.model = UNet(channels = n_channels)
+        self.encodec = WrappedEncodec()
         self.alpha_steps = alpha_steps
-        self._config_prior(torch.zeros(n_channels), torch.ones(n_channels))
+        self._config_prior(torch.zeros(n_channels, 1), torch.ones(n_channels, 1))
 
     def _config_prior(self, mean, std):
         self.mean = mean
@@ -18,7 +20,7 @@ class UNetDiffusion(nn.Module):
     def forward(self, x, t):
         return self.model(x, t)
     
-    def inference(self, x_0 = None, n_batch = 1, n_frames = 100, T = None):
+    def inference(self, x_0 = None, n_batch = 1, n_frames = 128, T = None):
         if T is None or not isinstance(T, int):
             T = self.alpha_steps
         if x_0 is None:
@@ -26,14 +28,14 @@ class UNetDiffusion(nn.Module):
         denoised_samples = [x_0]
         alpha = torch.arange(T)/T
         for t in range(1,T,1):
-            x_a = denoised_samples[-1] + (alpha[t] - alpha[t-1])*self.forward(denoised_samples[-1],alpha[t]*torch.ones(n_batch,1))
+            x_a = denoised_samples[-1] + (alpha[t] - alpha[t-1])*self.forward(denoised_samples[-1],alpha[t]*torch.ones(n_batch))
             denoised_samples.append(x_a)
-        return denoised_samples
+        return self.encodec.decode(denoised_samples[-1])
     
     def sample(self, n_batch, n_frames):
         return self.mean + self.std * torch.randn((n_batch, self.n_channels, n_frames))
     
-    def interpolate(self, x_0 = None, x_1 = None, n_batch = 1, n_inter = 10, n_frames = 100, T = None):
+    def interpolate(self, x_0 = None, x_1 = None, n_batch = 1, n_inter = 10, n_frames = 128, T = None):
         if x_0 is None:
             x_0 = self.sample(n_batch, n_frames)
         if x_1 is None:
