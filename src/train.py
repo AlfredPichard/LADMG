637c9cab5cb7cf021a234fb5c17a546f70c3d688
Paul Triana <paul.triana@telecom-paris.fr>
2023-12-30 01:08:56 +0100

    save/load in trainer and example of config json

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
+import utils
 
 class Trainer:
 
@@ -11,8 +12,9 @@ class Trainer:
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
         self.logger.add_graph(self.model)
+        self.epoch = 0
 
-    def train_one_epoch(self, epoch = 0, log = False, inference = False):
+    def train_one_epoch(self, log = False, inference = False):
         running_loss = 0.0
         batch_size = self.train_dataloader.batch_size
         for i, data in enumerate(self.train_dataloader):
@@ -27,15 +29,22 @@ class Trainer:
             loss.backward()
             self.optimizer.step()
             running_loss += loss.item()
-
+        
         if log:
             self.logger.add_scalar('training loss',
                             running_loss / i,
-                            epoch * len(self.train_dataloader) + i)
+                            self.epoch * len(self.train_dataloader) + i)
             running_loss = 0.0
         if inference:
             audio = self.model.inference()
-            self.logger.add_audio('inference epoch ' + epoch, audio, sample_rate = self.model.encodec.sr)
+            self.logger.add_audio('inference epoch ' + self.epoch, audio, sample_rate = self.model.encodec.sr)
             self.logger.flush()
             self.logger.close()
+        self.epoch += 1
 
+    def checkpoint(self):
+        name = self.model.name + "_e" + self.epoch
+        utils.save_checkpoint(self.model, name)
+
+    def load(self, model_path):
+        utils.load_checkpoint(self.model, model_path)
--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,9 @@ import torch
 import os
 from datetime import datetime
 
-def save_model(model, model_name, dir_path = "../saved_models", with_date=True):
+def save_checkpoint(model, model_name, dir_path = "../saved_models", with_date=True):
     if with_date:
-        supp = datetime.now().strftime("%S_%M_%H_%d_%m_*Y")
+        supp = datetime.now().strftime("%S_%M_%H_%d_%m_%Y")
     else:
         supp = ""
 
@@ -13,3 +13,7 @@ def save_model(model, model_name, dir_path = "../saved_models", with_date=True):
     torch.save(obj=model.state_dict(),
              f=model_path)
 
+    return model_path
+
+def load_checkpoint(model, model_path):
+    model.load_state_dict(torch.load(model_path))
