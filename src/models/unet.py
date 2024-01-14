aef8278d8659aca8f3fe8fa08084c74c863886dc
AlfredPichard <alfred.pichard@free.fr>
2024-01-14 14:30:45 +0100

    (apichard) add conditioning in unet

--- src/models/unet.py
+++ src/models/unet.py
@@ -6,7 +6,7 @@ from modules import EncodeBlock, DecodeBlock, BottomBlock, OutBlock, PositionalE
 
 class UNet(nn.Module):
 
-    def __init__(self, channels=2, time_emb_dim=64, start_channels_base2=6, n_layers=5, kernel_size_base2=1, n_groups=None, device='cpu'):
+    def __init__(self, channels=2, time_emb_dim=64, start_channels_base2=6, n_layers=5, kernel_size_base2=1, n_groups=None, num_classes=None, device='cpu'):
         super(UNet, self).__init__()
         self.n_layers = n_layers
 
@@ -50,10 +50,16 @@ class UNet(nn.Module):
 
         self.last_conv = nn.Conv1d(2**(start_channels_base2), channels, 1, device=device)
         self.pos_encoding = PositionalEncoding(time_emb_dim, device = device)
+
+        if num_classes is not None:
+            self.label_embedding = nn.Embedding(num_classes, time_emb_dim)
         
-    def forward(self, x, t):
+    def forward(self, x, t, y=None):
         pos_enc = self.pos_encoding(t)
         skip_connections = []
+
+        if y is not None:
+            t += self.label_embedding(y)
 
         for i in range(self.n_layers - 1):
             x, skip = self.encode[i](x, pos_enc)
